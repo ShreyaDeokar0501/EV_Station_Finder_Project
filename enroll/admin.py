415c3f1232da7e61dd54cb0b22dc4cbb9194c122
unknown <shreya0501@gmail.com>
2024-01-25 18:01:49 +0530

    files added

---
+++ enroll/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from enroll.models import *
+# Register your models here.
+admin.site.register(newuser)
+admin.site.register(Stationowner)
+admin.site.register(Stations)
+admin.site.register(Bookslots)
+admin.site.register(Payment)
+admin.site.register(Review)
